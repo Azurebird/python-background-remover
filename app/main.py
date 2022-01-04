59106bf4093109a33cfc37d00d674b12d1b4c9fa
Azurebird <white.camilo@gmail.com>
2022-01-04 18:57:29 -0500

    Checking mediapipe docs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+import cv2
 import mediapipe as mp
 import numpy as np
 
+# Initialize mediapipe
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
 
-if __name__ == "__main__":
-    print("Hello World!!")
+# Store background images in a list
+bg_image = cv2.imread("images/cafe.jpeg")
+
+# Create videocapture object to access the webcam
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+cap.release()
+cv2.destroyAllWindows()
